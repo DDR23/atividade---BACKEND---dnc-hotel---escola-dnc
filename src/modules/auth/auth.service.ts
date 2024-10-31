942e3e472ba954aebd6ab101de341fe72cdb7ac4
DDR23 <roboreserva.andre@gmail.com>
2024-10-31 20:50:15 -0300

    feat: adicionando rota de ragister

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { User } from "@prisma/client";
+import { Role, User } from "@prisma/client";
 import { AuthLoginDTO } from "./domain/dto/authLogin.dto";
 import { PrismaService } from "../prisma/prisma.service";
 import * as bcrypt from "bcrypt";
 import { UserService } from "../users/user.service";
+import { AuthRegisterDTO } from "./domain/dto/authRegister.dto";
+import { CreateUserDTO } from "../users/domain/dto/createUser.dto";
 
 @Injectable()
 export class AuthService {
@@ -23,6 +25,17 @@ export class AuthService {
   async login({ USER_EMAIL, USER_PASSWORD }: AuthLoginDTO): Promise<{ access_token: string }> {
     const user = await this.userService.findByEmail(USER_EMAIL);
     if (!user || !(await bcrypt.compare(USER_PASSWORD, user.USER_PASSWORD))) throw new UnauthorizedException('Email or password is incorrect');
+    return await this.generateJwtToken(user);
+  }
+
+  async register(body: AuthRegisterDTO): Promise<{ access_token: string }> {
+    const newUser: CreateUserDTO = {
+      USER_NAME: body.USER_NAME,
+      USER_PASSWORD: body.USER_PASSWORD,
+      USER_EMAIL: body.USER_EMAIL,
+      USER_ROLE: body.USER_ROLE ?? Role.USER,
+    }
+    const user = await this.userService.create(newUser)
     return await this.generateJwtToken(user);
   }
 }
