992920f21ca8434903b05060d9dce724a826b8f7
omegatech-andre <omega.ti_ind@outlook.com>
2024-10-31 13:51:51 -0300

    feat: Adicionando base das rotas de user

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller('users')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
+
+  @Post('create')
+  create(@Body() body: any) {
+    return this.userService.create(body);
+  }
 
   @Get()
-  getUser() {
-    return this.userService.getUsers();
+  list() {
+    return this.userService.list();
+  }
+
+  @Get(':id')
+  show(@Param('id') id: string) {
+    return this.userService.show(id);
+  }
+
+  @Patch('update/:id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.userService.update(id, body)
+  }
+
+  @Delete('delete/:id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id)
   }
 }
