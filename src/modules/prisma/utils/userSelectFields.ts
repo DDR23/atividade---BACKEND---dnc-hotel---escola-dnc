627ee45f417091d96b8fe564e8d983864b413d2f
omegatech-andre <omega.ti_ind@outlook.com>
2024-10-31 16:48:04 -0300

    feat: adicionando userSelectFields definir campos que serao mostrado no retorno da requisição

---
+++ src/modules/prisma/utils/userSelectFields.ts
@@ -0,0 +1,9 @@
+export const userSelectedFields = {
+  id: true,
+  USER_NAME: true,
+  USER_PASSWORD: false,
+  USER_EMAIL: true,
+  USER_ROLE: true,
+  createdAt: true,
+  updatedAt: false
+}
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -4,13 +4,14 @@ import { User } from "@prisma/client";
 import { CreateUserDTO } from "./domain/dto/createUser.dto";
 import { UpdateUserDTO } from "./domain/dto/updateUser.dto";
 import * as bcrypt from "bcrypt"
+import { userSelectedFields } from "../prisma/utils/userSelectFields";
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) { }
 
   async list(): Promise<User[]> {
-    return await this.prisma.user.findMany();
+    return await this.prisma.user.findMany({ select: userSelectedFields });
   }
 
   async show(id: number): Promise<User> {
@@ -20,13 +21,13 @@ export class UserService {
 
   async create(body: CreateUserDTO): Promise<User> {
     body.USER_PASSWORD = await this.hashPassword(body.USER_PASSWORD);
-    return await this.prisma.user.create({ data: body })
+    return await this.prisma.user.create({ data: body, select: userSelectedFields })
   }
 
   async update(id: number, body: UpdateUserDTO): Promise<User> {
     await this.isIdExists(id);
     if (body.USER_PASSWORD) body.USER_PASSWORD = await this.hashPassword(body.USER_PASSWORD);
-    return await this.prisma.user.update({ where: { id }, data: body });
+    return await this.prisma.user.update({ where: { id }, data: body, select: userSelectedFields });
   }
 
   async delete(id: number): Promise<User> {
@@ -35,12 +36,12 @@ export class UserService {
   }
 
   private async isIdExists(id: number): Promise<User> {
-    const user = await this.prisma.user.findUnique({ where: { id } });
+    const user = await this.prisma.user.findUnique({ where: { id }, select: userSelectedFields });
     if (!user) throw new NotFoundException('User not found')
     return user;
   }
 
-  private async hashPassword(password: string) {
+  private async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 8);
   }
 }
