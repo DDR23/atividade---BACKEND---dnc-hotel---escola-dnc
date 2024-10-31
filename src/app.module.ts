fffce117ce6232f3d10ee089e36f04de7c24c7a7
omegatech-andre <omega.ti_ind@outlook.com>
2024-10-31 11:02:08 -0300

    feat: adicionando user module controller e service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   // controllers: [], //nao utilizado aqui
   // providers: [], //nao utilizado aqui
 })
