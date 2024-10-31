b0e22d71f21da78ac9a41d365cf53f3613822872
omegatech-andre <omega.ti_ind@outlook.com>
2024-10-31 15:27:42 -0300

    feat: adicionando middleware de checagem de id

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,7 +1,8 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { PrismaModule } from "../prisma/prisma.module";
+import { UserIdCheckMiddleware } from "../shared/middlewares/userIdCheck.middleware";
 
 @Module({
   imports: [PrismaModule],
@@ -9,4 +10,13 @@ import { PrismaModule } from "../prisma/prisma.module";
   providers: [UserService],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserIdCheckMiddleware).forRoutes(
+      { path: 'users/:id', method: RequestMethod.GET },
+      { path: 'users/update/:id', method: RequestMethod.PATCH },
+      { path: 'users/delete/:id', method: RequestMethod.DELETE }
+    );
+    // more consumers...
+  }
+}
