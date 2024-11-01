b1a6459220e18531d3c8b52d7cc1ee751d89c98e
omegatech-andre <omega.ti_ind@outlook.com>
2024-11-01 11:02:23 -0300

    feat: adicionando cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    // EXEMPLOS DE OPTIONS DO CORS
+    // origin: 'http://localhost:3000',
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // allowedHeaders: 'Content-Type, Accept',
+    // credentials: true,
+  });
   // app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
