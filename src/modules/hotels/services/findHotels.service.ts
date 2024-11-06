57792503e739daf7679122a198d8751d5c3db5bc
omegatech-andre <omega.ti_ind@outlook.com>
2024-11-06 09:14:17 -0300

    feat: adicionando cache redis em find all de hotels

--- src/modules/hotels/services/findHotels.service.ts
+++ src/modules/hotels/services/findHotels.service.ts
@@ -1,16 +1,24 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IHotelRepositories } from '../domain/repositories/IHotel.repositories';
+import Redis from 'ioredis';
+import { InjectRedis } from '@nestjs-modules/ioredis';
 
 @Injectable()
 export class FindHotelsService {
   constructor(
     @Inject('HOTEL_SERVICE_TOKEN')
     private readonly hotelRepositories: IHotelRepositories,
+    @InjectRedis()
+    private readonly redis: Redis,
   ) { }
 
   async execute(page: number = 1, limit: number = 10) {
-    const offSet = (page -1) * limit;
-    const data = await this.hotelRepositories.findHotels(offSet, limit);
+    const offSet = (page - 1) * limit;
+    let data = JSON.parse(await this.redis.get('REDIS_HOTEL_KEY'));
+    if (!data) {
+      data = await this.hotelRepositories.findHotels(offSet, limit);
+      await this.redis.set('REDIS_HOTEL_KEY', JSON.stringify(data));
+    }
     const total = await this.hotelRepositories.countHotels();
     return {
       total,
